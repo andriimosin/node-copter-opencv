b3c598abf02fa28f211689d9328a4688a3dfc6ff
Andrey Mosin <mosinandrej@gmail.com>
2015-02-21 23:05:49 +0200

    Init

--- examples/drone-camera.js
+++ examples/drone-camera.js
@@ -16,10 +16,13 @@ pngStream
       if (err) throw err;
       if (im.width() < 1 || im.height() < 1) throw new Error('Image has no size');
 
+      var detectedImg = false;
+
       im.detectObject("../data/haarcascade_frontalface_alt.xml", {}, function(err, faces){
         if (err) throw err;
 
         for (var i = 0; i < faces.length; i++){
+          detectedImg = true;
           var face = faces[i];
           im.ellipse(face.x + face.width / 2, face.y + face.height / 2, face.width / 2, face.height / 2);
           im.save('./tmp/face-detection' + new Date().getTime() / 1000 + '.png');
@@ -37,7 +40,11 @@ var server = http.createServer(function(req, res) {
   }
 
   res.writeHead(200, {'Content-Type': 'image/png'});
-  res.end(lastPng);
+  if (!detectedImg) {
+    res.end(lastPng);
+  } else {
+    res.end(detectedImg);
+  }
 });
 
 server.listen(8080, function() {
